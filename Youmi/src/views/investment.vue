404b29feb35eeb611ef520b0ec7ed9c628af79bb
COSMIC-Z <928189059@qq.com>
2025-02-21 17:55:34 +0800

    有米科技2025.02.21

---
+++ Youmi/src/views/investment.vue
@@ -0,0 +1,18 @@
+<!-- 产品招商 -->
+
+<template>
+    <div class="home">
+        <h1>产品招商</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .home {
+        width: 100%;
+        height: 800px;
+    }
+</style>
---
+++ Youmi/src/views/plan.vue
@@ -0,0 +1,18 @@
+<!-- 产园规划 -->
+
+<template>
+    <div class="home">
+        <h1>产园规划</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .home {
+        width: 100%;
+        height: 800px;
+    }
+</style>
