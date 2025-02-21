f0c680b532c58ef0fc77dfea74ce04e5c8767a09
COSMIC-Z <928189059@qq.com>
2025-02-21 18:11:49 +0800

    有米2025.02.21

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="app">
+    <Headerview/>
+    <!-- 展示路由 -->
+    <router-view/>
+    <Bottom/>
+  </div>
+</template>
+
+<script lang="ts">
+//引入组件
+import Headerview from './components/header.vue'
+import Bottom from './components/bottom.vue'
+export default {
+	name:'App',
+  components:{
+    Headerview,
+    Bottom
+  }
+}
+</script>
+
+<style>
+.app{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
