e68cff334f90f035a5b91eb5b5f7a6bb8ac1adbc
COSMIC-Z <928189059@qq.com>
2025-02-22 16:26:08 +0800

    更新底边栏

--- src/views/operate.vue
+++ src/views/operate.vue
@@ -1,15 +1,18 @@
 <!-- 众创运营 -->
+
 <template>
-  <div class="operate">
-    <div class="bannerWrap"></div>
-  </div>
+    <div class="home">
+        <h1>众创运营</h1>
+    </div>
 </template>
 
-<script></script>
+<script>
 
-<!-- <style lang="scss" scoped>
-.home {
-  width: 100%;
-  height: 800px;
-}
-</style> -->
+</script>
+
+<style>
+    .home {
+        width: 100%;
+        height: 800px;
+    }
+</style>
